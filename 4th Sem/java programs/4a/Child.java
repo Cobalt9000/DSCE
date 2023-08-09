bb6a714a7008400afbcdde1d26fc82473e990e72
Cob@lt9000 <110296623+Cobalt9000@users.noreply.github.com>
2023-08-09 22:27:18 +0530

    single inheritence

---
+++ 4th Sem/java programs/4a/Child.java
@@ -0,0 +1,17 @@
+package Single_Inheritance;
+
+//Child.java
+public class Child extends Parent {
+ public void bike() {
+     System.out.println("Bike");
+ }
+ 
+ public static void main(String[] args) {
+     Child c = new Child();
+     c.house();
+     c.car();
+     c.bike();
+     c.garden();
+ }
+}
+
---
+++ 4th Sem/java programs/4a/Parent.java
@@ -0,0 +1,16 @@
+package Single_Inheritance;
+
+import Multi_Inheritance.Grandparent;
+
+//Parent.java
+public class Parent extends Grandparent {
+ public void house() {
+     System.out.println("House");
+ }
+ 
+ public void car() {
+     System.out.println("Car");
+ }
+}
+
+
